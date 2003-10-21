15a98358f6eb92a19b457d709322b03836ab4147
Nigel Stewart <nigels@users.sourceforge.net>
2003-10-21 12:24:01 +0000

    Code formatting improvements - no functional change

--- viewmodel.h
+++ viewmodel.h
@@ -32,12 +32,12 @@
      vm.update();  // updates the three vectors 
 
      vm.move( 0.0, 2.0, 0.0 );  // Moves the eye and lookat 
-				   by 2 units in the world 
-				   Y direction 
+                   by 2 units in the world 
+                   Y direction 
      vm.roll( 5.0 );            // rolls 5 degrees about the forward
-				   axis 
+                   axis 
      vm.lookat_yaw( -25.0 );    // Yaws about the eye (lookat is
-				   fixed) by -25 degrees 
+                   fixed) by -25 degrees 
      vm.load_to_openGL();       // Sets OpenGL modelview matrix 
      
      .. render ...
@@ -55,7 +55,7 @@
   lookat_yaw()     - Rotates lookat about up vector
   lookat_pitch()   - Rotates lookat about side vector
   reset_up_level() - Resets the up vector (after unwanted rolls), and 
-	                   sets the eye level with the lookat point
+                       sets the eye level with the lookat point
   move()           - Moves eye and lookat by some amount
   move_by_eye()    - Moves eye to new position, lookat follows
   move_by_lookat() - Moves lookat to new position, eye follows
@@ -73,20 +73,18 @@
   dump()           - Prints class contents to a file
 
 
-  - 1996 Paul Rademacher (rademach@cs.unc.edu)
+      1996, Paul Rademacher (rademach@cs.unc.edu)
+  Oct 2003, Nigel Stewart - GLUI Code Cleaning
 
 *********************************************************************/
 
 #ifndef _VIEW_MODEL_H_
 #define _VIEW_MODEL_H_
 
-#include "algebra3.h" /* Include algebra3.h first to avoid Windows 
-			 problems */
-#include <GL/glui.h>
-#include <cstdio>
-#include <cstdlib>
+#include "algebra3.h"
 
-class ViewModel {
+class ViewModel 
+{
 public:
   vec3    eye, lookat;
   vec3    up, side, forward;
@@ -97,341 +95,130 @@ public:
   /* This readjusts the distance from the eye to the lookat */
   /* (changing the eye point in the process)                */
   /* The lookat point is unaffected                         */
-  void set_distance( float new_distance ) {
-    if ( new_distance <= 0.0 )  /* Distance has to be positive */
-      return;
-
-    /* We find the current forward vector */
-    forward = lookat - eye;
-    forward.normalize();
-    
-    /* Set distance */
-    distance = new_distance;
-
-    /* Find new eye point */
-    eye = lookat - forward * distance;
-  }
+  void set_distance(float new_distance);
 
   /******************************* set_up() ***************/
-  void set_up( vec3 new_up ) {
-    up = new_up;
-    update();
-  }
+  void set_up(vec3 new_up);
 
-  void set_up( float x, float y, float z ) {
-    set_up(vec3(x,y,z));
-  }
+  void set_up(float x, float y, float z);
 
   /******************************* set_eye() ***************/
-  void set_eye( vec3 new_eye ) {
-    eye = new_eye;
-    update();
-  }
+  void set_eye( vec3 new_eye );
 
-  void set_eye( float x, float y, float z ) {
-    set_eye(vec3(x,y,z));
-  }
+  void set_eye(float x, float y, float z);
 
   /******************************* set_lookat() ***************/
-  void set_lookat( vec3 new_lookat ) {
-    lookat = new_lookat;
-    update();
-  }
+  void set_lookat(vec3 new_lookat);
 
-  void set_lookat( float x, float y, float z ) {
-    set_lookat(vec3(x,y,z));
-  }
-
+  void set_lookat(float x, float y, float z);
 
   /******************************* roll() *****************/
   /* Rotates about the forward vector                     */
   /* eye and lookat remain unchanged                      */
-  void roll( float angle ) {
-    mat4 rot = rotation3D( forward, angle );
-
-    up = rot * up;
-
-    update();
-  }
+  void roll(float angle);
 
   /******************************* eye_yaw() *********************/
   /* Rotates the eye about the lookat point, using the up vector */
-  /* Lookat is unaffected                                 */
-  void eye_yaw( float angle ) {
-    vec3 eye_pt = eye - lookat; /* eye w/lookat at center */
-    mat4 rot    = rotation3D( up, angle );
-    
-    eye_pt = rot * eye_pt;
-    eye    = lookat + eye_pt;
-     
-    update();
-  }
+  /* Lookat is unaffected                                        */
+  void eye_yaw(float angle);
 
   /******************************* eye_yaw_abs() ******************/
   /* Rotates the eye about the lookat point, with a specific axis */
-  /* Lookat is unaffected                                 */
-  void eye_yaw_abs( float angle, vec3 axis ) {
-    vec3 eye_pt   = eye  - lookat;   /* eye w/lookat at center */
-    mat4 rot      = rotation3D( axis, angle );
-    
-    eye_pt  = rot * eye_pt;
-    eye     = lookat + eye_pt;
-
-    up      = rot * up;
-
-    update();
-  }
+  /* Lookat is unaffected                                         */
+  void eye_yaw_abs(float angle, vec3 axis);
 
 
   /******************************* eye_pitch() ************/
   /* Rotates the eye about the side vector                */
   /* Lookat is unaffected                                 */
-  void eye_pitch( float angle ) {
-    vec3 eye_pt = eye - lookat; /* eye w/lookat at center */
-    mat4 rot    = rotation3D( side, angle );
-    
-    eye_pt = rot * eye_pt;
-    eye    = lookat + eye_pt;
-
-    up = rot * up;
-     
-    update();
-  }
-
+  void eye_pitch(float angle);
 
   /******************************* lookat_yaw()************/
   /* This assumes the up vector is correct.               */
   /* Rotates the lookat about the side vector             */
   /* Eye point is unaffected                              */
-  void lookat_yaw( float angle ) {
-    vec3 lookat_pt = lookat - eye;   /* lookat w/eye at 
-					center */
-    mat4 rot = rotation3D( up, -angle );
-    
-    lookat_pt = rot * lookat_pt;
-    lookat = eye + lookat_pt;
-
-    update();
-  }
-
-  
+  void lookat_yaw(float angle);
 
   /******************************* lookat_pitch() *********/
   /* Rotates the lookat point about the side vector       */
   /* This assumes the side vector is correct.             */
   /* Eye point is unaffected                              */
-  void lookat_pitch( float angle ) {
-    vec3 lookat_pt = lookat - eye;   /* lookat w/eye at 
-					center */
-    mat4 rot = rotation3D( side, -angle );
-    
-    lookat_pt = rot * lookat_pt;
-    lookat = eye + lookat_pt;
-
-    up = rot * up;
-
-    update();
-  }
-
+  void lookat_pitch(float angle);
 
   /******************************* reset_up() ******************/
   /* Resets the up vector to a specified axis (0=X, 1=Y, 2=Z)  */
   /* Also sets the eye point level with the lookat point,      */
   /* along the specified axis                                  */
-  void reset_up( int axis_num ) {
-    float eye_distance = (lookat - eye).length();
-    eye[axis_num] = lookat[axis_num]; 
-    /* Bring eye to same level as lookat */
+  void reset_up(int axis_num);
 
-    vec3 vector = eye - lookat;
-    vector.normalize();
-    vector *= eye_distance;
+  void reset_up();
 
-    eye = lookat + vector;
-    up.set( 0.0, 0.0, 0.0 );
-    up[axis_num] = 1.0;
-
-    update();
-  }
-
-  void reset_up( void ) {
-    reset_up( VY ); /* Resets to the Y axis */
-  }
-
-  /******************************* move() *****************/
+  /******************************* move() ********************/
   /* Moves a specified distance in the forward, side, and up */
   /* directions.  This function does NOT move by world       */
   /* coordinates.  To move by world coords, use the move_abs */
   /* function.                                               */
-  void move( float side_move, float up_move, float forw_move ) {
-    eye += forward * forw_move;
-    eye += side    * side_move;
-    eye += up      * up_move;
-    lookat += forward * forw_move;
-    lookat += side    * side_move;
-    lookat += up      * up_move;
-    update();
-  }
+  void move(float side_move, float up_move, float forw_move);
 
-  void move( vec3 v ) { /* A vector version of the above command */
-    move( v[VX], v[VY], v[VZ] );
-  }
+  void move(vec3 v);
 
   /******************************* move_by_eye() ***********/
   /* Sets the eye point, AND moves the lookat point by the */
   /* same amount as the eye is moved.                      */
-  void move_by_eye( vec3 new_eye ) {
-    vec3 diff = new_eye - eye;
-
-    eye    += diff;
-    lookat += diff;
-
-    update();
-  }
+  void move_by_eye(vec3 new_eye);
 
   /******************************* move_by_lookat() *********/
   /* Sets the lookat point, AND moves the eye point by the  */
   /* same amount as the lookat is moved.                    */
-  void move_by_lookat( vec3 new_lookat ) {
-    vec3 diff = new_lookat - lookat;
-
-    lookat += diff;
-    eye    += diff;
-
-    update();
-  }
+  void move_by_lookat(vec3 new_lookat);
 
   /******************************* move_abs() *****************/
   /* Move the eye and lookat in world coordinates             */
-  void move_abs( vec3 v ) {
-    eye    += v;
-    lookat += v;
-
-    update();
-  }
+  void move_abs(vec3 v);
 
   /****************************** rot_about_eye() ************/
   /* Rotates the lookat point about the eye, based on a 4x4  */
   /* (pure) rotation matrix                                  */
-  void rot_about_eye( mat4 rot ) {
-    vec3  view = lookat - eye;
-
-    view = rot * view;
-    up   = rot * up;
-
-    lookat = eye + view;
-
-    update();
-  }
+  void rot_about_eye(mat4 rot);
 
   /****************************** rot_about_lookat() ************/
   /* Rotates the lookat point about the lookat, based on a 4x4  */
   /* (pure) rotation matrix                                  */
-  void rot_about_lookat( mat4 rot ) {
-    // NOT QUITE RIGHT YET
-
-
-	 vec3 view = eye - lookat;
-
-    view = rot * view;
-    up   = rot * up;
-
-    eye = lookat + view;
-
-    update();
-  }
+  void rot_about_lookat(mat4 rot);
 
   /******************************* make_mtx() *************/
   /* Constructs a 4x4 matrix - used by load_to_openGL()   */
-  void make_mtx( void ) {
-    update();
+  void make_mtx();
 
-    mtx[0][0]=side[VX]; mtx[0][1]=up[VX]; mtx[0][2]=forward[VX]; mtx[0][3]=0.0;
-    mtx[1][0]=side[VY]; mtx[1][1]=up[VY]; mtx[1][2]=forward[VY]; mtx[1][3]=0.0;
-    mtx[2][0]=side[VZ]; mtx[2][1]=up[VZ]; mtx[2][2]=forward[VZ]; mtx[2][3]=0.0;
-    mtx[3][0]=0.0;     mtx[3][1]=0.0;   mtx[3][2]= 0.0;        mtx[3][3]=1.0;
-  }
-
-  /******************************* load_to_openGL() *******/
+  /******************************* load_to_openGL() ********/
   /* Sets the OpenGL modelview matrix based on the current */
   /* camera coordinates                                    */
-  void load_to_openGL( void ) {
-    int   i;
-    mat4  m;
-
-    make_mtx();
-
-    glMatrixMode( GL_MODELVIEW );
-    glLoadIdentity();
-    glMultMatrixf( (float*) &mtx[0][0]);
-    glTranslatef( -eye[VX], -eye[VY], -eye[VZ] );
-  }
+  void load_to_openGL();
 
   /******************************* load_to_openGL_noident() ******/
   /* Multiplies the current camera matrix by the existing openGL */
   /* modelview matrix.  This is same as above function, but      */
   /* does not set the OpenGL matrix to identity first            */
-  void load_to_openGL_noident( void ) {
-    int   i;
-    mat4  m;
-
-    make_mtx();
-
-    glMatrixMode( GL_MODELVIEW );
-    glMultMatrixf( (float*) &mtx[0][0]);
-    glTranslatef( -eye[VX], -eye[VY], -eye[VZ] );
-  }
+  void load_to_openGL_noident();
 
   /******************************* reset() ****************/
   /* Resets the parameters of this class                  */
-  void reset( void ) {
-    up.set( 0.0, 1.0, 0.0 );
-
-    eye.set(0.0,0.0,10.0);
-    lookat.set(0.0,0.0,0.0);
-
-    mtx = identity3D();
-
-    update();
-  }
+  void reset();
 
   /******************************* ViewModel() ************/
   /* Constructor                                          */
-  ViewModel( void ) {
-    reset();
-  }
+  ViewModel();
 
   /******************************* update() ****************/
   /* updates the view params.  Call this after making      */
   /* direct changes to the vectors or points of this class */
-  void update( void ) {
-    /* get proper side and forward vectors, and distance  */
-    forward  = -(lookat - eye);
-    distance = forward.length();
-    forward /= distance;
-
-    side     = up ^ forward;
-    up       = forward ^ side;
-
-    forward.normalize();
-    up.normalize();
-    side.normalize();
-  }
+  void update();
 
 
   /******************************* dump() *******************/
   /* Prints the contents of this class to a file, typically */
   /* stdin or stderr                                        */
-  void dump( FILE *output ) {
-    fprintf( output, "Viewmodel: \n" );
-    eye.print(    output, "  eye"    );
-    lookat.print( output, "  lookat" );
-    up.print(     output, "  up"     );
-    side.print(   output, "  side"   );
-    forward.print(output, "  forward");
-    mtx.print(    output, "  mtx"    );
-  }
+  void dump(FILE *output);
 };
-
 
 #endif
