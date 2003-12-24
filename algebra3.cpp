360c6e1b0cdc63aca01033bec1411eadf0939f93
Nigel Stewart <nigels@users.sourceforge.net>
2003-12-24 00:38:19 +0000

    Updated license information in each source file to clarify that GLUI is LGPL.

--- algebra3.cpp
+++ algebra3.cpp
@@ -1,7 +1,31 @@
+/*
+
+  algebra3.cpp, algebra3.h -  C++ Vector and Matrix Algebra routines
+
+  GLUI User Interface Toolkit (LGPL)
+  Copyright (c) 1998 Paul Rademacher
+
+  WWW:    http://sourceforge.net/projects/glui/
+  Forums: http://sourceforge.net/forum/?group_id=92496
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 /**************************************************************************
     
-  algebra3.cpp, algebra3.h -  C++ Vector and Matrix Algebra routines           
-
   There are three vector classes and two matrix classes: vec2, vec3,
   vec4, mat3, and mat4.
 
