f601fc6eda8d1cf1c50c7275f6f7d6900d29977c
Nigel Stewart <nigels@users.sourceforge.net>
2005-03-27 06:16:18 +0000

    Formatting improvements to GLUI 2.3 widget documentation

--- example6.cpp
+++ example6.cpp
@@ -28,120 +28,121 @@ GLUI_TreePanel   *tp;
 GLUI_FileBrowser *fb;
 GLUI_EditText    *bedit;
 
-char general[] = "GLUI 2.3 Widgets\n\
-\n\
-A number of widgets have been introduced in GLUI 2.3\n\
-\n\
-\t* GLUI_Scrollbar \t- A scrollbar slider widget\n\
-\t* GLUI_TextBox \t- A multi-line text widget\n\
-\t* GLUI_List \t- A static choice list\n\
-\t* GLUI_FileBrowser \t- A simple filebrowser based on GLUI_List\n\
-\t* GLUI_Tree \t- Hierarchical tree widget\n\
-\t* GLUI_TreePanel \t- Manager for the tree widget\n\
-";
+const char general[] =
+  "GLUI 2.3 Widgets\n"
+  "\n"
+  "A number of widgets have been introduced in GLUI 2.3\n"
+  "\n"
+  "  * GLUI_Scrollbar   - A scrollbar slider widget\n"
+  "  * GLUI_TextBox     - A multi-line text widget\n"
+  "  * GLUI_List        - A static choice list\n"
+  "  * GLUI_FileBrowser - A simple filebrowser based on GLUI_List\n"
+  "  * GLUI_Tree        - Hierarchical tree widget\n"
+  "  * GLUI_TreePanel   - Manager for the tree widget\n";
 
-char scroll[] = "Scrollbar Widget\n\
-\n\
-The GLUI_Scrollbar can be used more or less like a GLUI_Spinner.\n\
-It can be horizontal or vertical, and have float or int values.\n\
-\n\
-The set_int_limits and set_float_limits functions work properly even\n\
-if the min and max values are reversed.  In this way you can choose\n\
-whether the left/right or top/bottom is the minimum value.  \n\
-\n\
-For instance, with set_int_limits(0, 100) you get a normal slider with\n\
-0 corresponding to the left (bottom) end, and 100 corresponding to the\n\
-right (top) end.\n\
-\n\
-By calling set_int_limits(100,0) instead, the right (top) end becomes 0,\n\
-and the left (bottom) end is 100.\n\
-";
+const char scroll[] = 
+  "Scrollbar Widget\n"
+  "\n"
+  "The GLUI_Scrollbar can be used more or less like a GLUI_Spinner.\n"
+  "It can be horizontal or vertical, and have float or int values.\n"
+  "\n"
+  "The set_int_limits and set_float_limits functions work properly even\n"
+  "if the min and max values are reversed.  In this way you can choose\n"
+  "whether the left/right or top/bottom is the minimum value.  \n"
+  "\n"
+  "For instance, with set_int_limits(0, 100) you get a normal slider with\n"
+  "0 corresponding to the left (bottom) end, and 100 corresponding to the\n"
+  "right (top) end.\n"
+  "\n"
+  "By calling set_int_limits(100,0) instead, the right (top) end becomes 0,\n"
+  "and the left (bottom) end is 100.\n";
 
-char tree_txt[] = "Tree Widget";
+const char tree_txt[] = "Tree Widget";
 
-char commandline[] = "CommandLine Widget\n\
-\n\
-The GLUI_CommandLine is a GLUI_EditText with some special functionality\n\
-useful for implementing a simple command-line style input widget.  \n\
-The main feature is that it maintains a command history buffer so that\n\
-users can recall previous commands.\n\
-";
+const char commandline[] = 
+  "CommandLine Widget\n"
+  "\n"
+  "The GLUI_CommandLine is a GLUI_EditText with some special functionality\n"
+  "useful for implementing a simple command-line style input widget.\n"
+  "The main feature is that it maintains a command history buffer so that\n"
+  "users can recall previous commands.\n";
 
-char gstring[] = "GLUI_String\n\
-\n\
-The GLUI string class used to be wrapper class around \n\
-a fixed 300 char buffer.\n\
-Now GLUI_String is just a typedef for std::string.\n\
-\n\
-One new function has been introduced, which is a sprintf() equivalent\n\
-for std::string/GLUI_String.  \n\
-\n\
-\tglui_format_str(GLUI_String& str, const char *format_str, ...);\n\
-\n\
-Other than the fact that it takes a GLUI_String as the first argument,\n\
-it works just like sprintf().\n\
-";
+const char gstring[] = 
+  "GLUI_String\n"
+  "\n"
+  "The GLUI string class used to be wrapper class around \n"
+  "a fixed 300 char buffer.\n"
+  "Now GLUI_String is just a typedef for std::string.\n"
+  "\n"
+  "One new function has been introduced, which is a sprintf() equivalent\n"
+  "for std::string/GLUI_String.\n"
+  "\n"
+  "  glui_format_str(GLUI_String& str, const char *format_str, ...);\n"
+  "\n"
+  "Other than the fact that it takes a GLUI_String as the first argument,\n"
+  "it works just like sprintf().\n";
 
-char list[] = "List Widget\n\
-\n\
-As seen on the left side of this window.";
+const char list[] =
+  "List Widget\n"
+  "\n"
+  "As seen on the left side of this window.";
 
-char filebrowser[] = "FileBrowserWidget\n\
-\n\
-As seen in the lower left corner of this window.\n\
-\n\
-The GLUI_FileBrowser is a subclass of GLUI_List and provides,\n\
-a simple file browsing and selecting interface under Windows and Unix.\n\
-\n\
-Go ahead and give it a try!  Navigate and select files by double-clicking.\n\
-If you pick a text file it will be displayed in this GLUI_TextBox.\n\
-";
+const char filebrowser[] = 
+  "FileBrowserWidget\n"
+  "\n"
+  "As seen in the lower left corner of this window.\n"
+  "\n"
+  "The GLUI_FileBrowser is a subclass of GLUI_List and provides,\n"
+  "a simple file browsing and selecting interface under Windows and Unix.\n"
+  "\n"
+  "Go ahead and give it a try!  Navigate and select files by double-clicking.\n"
+  "If you pick a text file it will be displayed in this GLUI_TextBox.\n";
 
-char text_box[] = "TextBox w/ optional Scrollbar\n\
-\n\
-A TextBox is a multiline editing box that supports basic navigation, \
-formatting, and editing capabilities. These are text block selection, \
-tabs, new lines, arrow key navigation, as well as an optional scrollbar. \
-The scrollbar is actually a distinct widget and can exist by itself, or \
-be associated with objects that have a specified callback routine.\n\
-\n\
-new GLUI_TextBox\n\
-Adds a new textbox to a GLUI window, optionally nested within another \n\
-rollout or panel.\n\
-\n\
-Usage:\n\
-new GLUI_TextBox(GLUI_Node *glui_or_panel, char *live_var=NULL,\n\
-\tint scroll = false,\n\
-\tint id=-1,\n\
-\tGLUI_Update_CB callback=NULL );\n\
-\n\
-\tpanel\t- Name of panel to place the TextBox in.\n\
-\tlive_var\t - Currently unimplemented, this might be used\n\t \t\
-in future iterations.\n\
-\tscroll\t - set to \"true\" if you want a scrollbar on the left side\n\
-\tid\t - If callback is defined, it will be passed this integer\n\t \t\
-when the value is modified.\n\
-\tcallback\t - Pointer to callback function of type (*)(int)\n\
-\n\
-Notes:\n\
-The scrollbar widget uses a different callback scheme to communicate with \
-the textbox. Any object wishing to utilize a scrollbar in some way should \
-implement a static function in the object with the signature:\n\
-\n\
-(*)(GLUI_Control *, int)\n\
-\n\
-Where the first parameter is the object the scrollbar is associated with \
-and the second is the int_val passed into the object by the scrollbar.\
-Refer to glui_textbox.cpp and glui_scrollbar.cpp for implementation details.\n\
-\n\
-Wishlist:\n\
-* Word Wrap\n\
-* Optimized Drawing (Update only lines that change, etc.)\n\
-* Multicolour text\n\
-* Current word selection\n\
-* copy/paste\n\
-\n\
-";
+const char text_box[] = 
+  "TextBox w/ optional Scrollbar\n"
+  "\n"
+  "A TextBox is a multiline editing box that supports basic navigation, \n"
+  "formatting, and editing capabilities. These are text block selection, \n"
+  "tabs, new lines, arrow key navigation, as well as an optional scrollbar. \n"
+  "The scrollbar is actually a distinct widget and can exist by itself, or \n"
+  "be associated with objects that have a specified callback routine.\n"
+  "\n"
+  "new GLUI_TextBox\n"
+  "Adds a new textbox to a GLUI window, optionally nested within another \n"
+  "rollout or panel.\n"
+  "\n"
+  "Usage:\n"
+  "new GLUI_TextBox(GLUI_Node *glui_or_panel, char *live_var=NULL,\n"
+  "  int scroll = false,\n"
+  "  int id=-1,\n"
+  "  GLUI_Update_CB callback=NULL );\n"
+  "\n"
+  "  panel    - Name of panel to place the TextBox in.\n"
+  "  live_var - Currently unimplemented, this might be used\n"
+  "             in future iterations.\n"
+  "  scroll   - set to \"true\" if you want a scrollbar on the left side\n"
+  "  id       - If callback is defined, it will be passed this integer\n"
+  "             when the value is modified.\n"
+  "  callback - Pointer to callback function of type (*)(int)\n"
+  "\n"
+  "Notes:\n"
+  "The scrollbar widget uses a different callback scheme to communicate with \n"
+  "the textbox. Any object wishing to utilize a scrollbar in some way should \n"
+  "implement a static function in the object with the signature:\n"
+  "\n"
+  "(*)(GLUI_Control *, int)\n"
+  "\n"
+  "Where the first parameter is the object the scrollbar is associated with \n"
+  "and the second is the int_val passed into the object by the scrollbar.\n"
+  "Refer to glui_textbox.cpp and glui_scrollbar.cpp for implementation details.\n"
+  "\n"
+  "Wishlist:\n"
+  "* Word Wrap\n"
+  "* Optimized Drawing (Update only lines that change, etc.)\n"
+  "* Multicolour text\n"
+  "* Current word selection\n"
+  "* copy/paste\n";
+  
 /*Bugs:\n\
  * fewer Mouse Drag calls  with large texts\n\
  * Insertion Pt sometimes drawn on wrong line.\n\
